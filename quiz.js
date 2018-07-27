c158269768b323f5f94ae3394c352b66837a246b
Irene <ireneho.imyh@gmail.com>
2018-07-27 13:21:49 -0700

    added showResults function

--- quiz.js
+++ quiz.js
@@ -89,10 +89,55 @@ function buildQuiz() {
 }
 
 function showResults() {
+  /* QUESTION: what steps should the program take to display results?
+  something like...
+  1. find the selected answer
+  2. handle what happens if answer is correct
+  3. handle what happens if answer is wrong
+  */
 
+  // to build results, we need to see the answers from the html file
+  // this selects all divisions from the div class "answers"
+  // QUESTION: how can we store our answers?
+  const answerContainers = quizContainer.querySelectorAll(".answers");
+
+  // keep track of user's answers
+  let numCorrect = 0;
+
+  // for each question...
+  myQuestions.forEach((currentQuestion, questionNumber) => {
+    // find selected answer
+    // we have to look at one answer container/box at a time
+    // QUESTION: how can we make sure we're looking in the right container?
+    const answerContainer = answerContainers[questionNumber];
+
+    // these 2 lines incorporate things that don't need to be delved into
+    // the important thing is that this is how to find which answer the user selected
+    // define a CSS selector to find which radio button was pressed
+    const selector = `input[name=question${questionNumber}]:checked`;
+    // use CSS selector to find which answer's radio button is pressed
+    const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+    // QUESTION: what happens if the user is correct/incorrect?
+    if (userAnswer === currentQuestion.correctAnswer) {
+      // add to number of correct answers
+      numCorrect++;
+
+      // color all the answers green
+      answerContainers[questionNumber].style.color = "lightgreen";
+    } else {
+      // if the answer is wrong/blank
+      // color answers red
+      answerContainers[questionNumber].style.color = "red";
+    }
+  });
+
+  // show number of correct answers out of total
+  // QUESTION: how should the results look?
+  resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
 }
 // build quiz right when the page loads
 buildQuiz();
 
 // on submit, show results
-submitButton.addEventListener('click', showResults);
+submitButton.addEventListener("click", showResults);
